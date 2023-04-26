7a0da5056d18a91af49d478df277463d6e0c5334
Luiz Batista Cardoso <luizbatistacardoso@gmail.com>
2023-04-26 10:19:44 -0300

    Aula 26/04 - Adicionando tabela depois Reverse Engineering
    Querry para o banco criando ( atualizando ) uma nova tabela chamada ranking, mesmo depois de já ter feito a geração do script e implementado o banco.

---
+++ trabalhoProjetoSQL/QUERRY-padel_db.sql
@@ -0,0 +1,28 @@
+show databases;
+use padel_db;
+
+show tables;
+
+select * from jogador;
+select * from torneio;
+select * from jogador_torneio;
+
+# Criando nova tabela depois de ja ter implementado
+CREATE TABLE IF NOT EXISTS Ranking (
+# Colunas
+  `idRanking` INT NOT NULL AUTO_INCREMENT,
+  `idJogador` INT NOT NULL,
+  `pontos` INT NOT NULL,
+# Chave primaria
+  PRIMARY KEY (`idRanking`),
+# Index de ordenação ( pontos maior - menor )  
+  INDEX `Index` (`pontos` DESC) VISIBLE,
+# Restrições da tabela ( não pode deletar ou atualizar o jogador )
+  CONSTRAINT `fk_Ranking_Jogador`
+    FOREIGN KEY (`idJogador`)
+    REFERENCES `Jogador` (`idJogador`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+select * from Ranking;
